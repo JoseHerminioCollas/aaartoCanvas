db312acd90658ce5616266b9d9654dc11efb0020
Jose Collas <joseherminiocollas@goatstone.com>
2024-08-15 16:18:24 -0400

    Build draw area and controls

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
+const makeCircle = (x, y) => {
+  const circleColor = "red";
+  const circleSize = 20;
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  path.setAttribute("r", circleSize);
+  path.setAttribute("cx", x);
+  path.setAttribute("cy", y);
+  path.setAttribute("fill", circleColor);
 
-console.log('aaarto')
+  return path;
+};
+const makeSquare = (x, y) => {
+  const squareLength = 20;
+  const squareColor = "blue";
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  path.setAttribute("width", squareLength);
+  path.setAttribute("height", squareLength);
+  path.setAttribute("x", x);
+  path.setAttribute("y", y);
+  path.setAttribute("fill", squareColor);
+
+  return path;
+};
+/**
+ * Initialize the application
+ */
+const init = () => {
+  const el = document.querySelector("#art");
+  el.addEventListener("click", (event) => {
+    const { clientX, clientY } = event;
+    const { top, left } = event.target.getBoundingClientRect();
+    const x = clientX - left;
+    const y = clientY - top;
+    el.append(makeCircle(x, y));
+    el.append(makeSquare(x, y));
+  });
+};
+window.addEventListener("load", init);
